fbbdbbac30fc19fe68724afc2f1c52f045c82943
AromatUspexa <rtomilkin@gmail.com>
2024-12-03 23:02:39 +0300

    Изменил обработку ошибок.

--- rambler_change/errors.py
+++ rambler_change/errors.py
@@ -1,6 +1,51 @@
 class RamblerException(Exception):
     pass
+
+
 class LoginFailed(RamblerException):
     pass
+
+
 class BanAccount(RamblerException):
     pass
+
+
+class PasswordChangeFailed(RamblerException):
+    pass
+
+
+class CaptchaError(RamblerException):
+    pass
+
+
+class CaptchaTaskCreationError(CaptchaError):
+    pass
+
+
+class CaptchaSolutionError(CaptchaError):
+    pass
+
+
+class BadDataAccount(RamblerException):
+    pass
+
+
+class PlaywrightError(RamblerException):
+    pass
+
+
+class ResponseCodeFailure(PlaywrightError):
+    pass
+
+
+class PageCloseError(PlaywrightError):
+    pass
+
+
+class AttemptsErrorReached(RamblerException):
+    pass
+
+
+
+
+
